5f04979a220f69030d3336f7f20c182f41240075
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2023-07-10 00:08:59 +0200

    feat: added findById

--- src/main/java/dev/fiks/controller/ContentController.java
+++ src/main/java/dev/fiks/controller/ContentController.java
@@ -1,8 +1,12 @@
 package dev.fiks.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 
 import dev.fiks.model.Content;
 import dev.fiks.repository.ContentCollectionRepository;
@@ -22,5 +26,13 @@ public class ContentController {
 	@GetMapping("")
 	public List<Content> findAll() {
 		return repository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Content findById(@PathVariable Integer id) {
+		return repository.findById(id).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+						String.format("Content with id %d not found", id))
+		);
 	}
 }
